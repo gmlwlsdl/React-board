d334f955c2c7a138421c9049b8ff0ba68ca2bb8d
gmlwlsdl <heejin094@naver.com>
2024-05-19 10:49:24 +0900

    modified eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
 import globals from 'globals';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import customConfig from './eslint.config.json';
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginReactConfig,
+  customConfig,
 ];
